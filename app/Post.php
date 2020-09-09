25136ff6e5018d4fe796de9ec28d334cffe4d2a8
Javier Amaya <Javieramayapat@gmail.com>
2020-09-08 23:59:43 -0500

    Store con TDD

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /* 
+        Creación de una propiedad de asignamiento masivo
+        que recibe como parámetro el titulo del post
+    */
+    protected $fillable = ['title'];
 }
--- tests/Feature/Http/Controllers/API/PostControllerTest.php
+++ tests/Feature/Http/Controllers/API/PostControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers\API;
+namespace Tests\Feature\Http\Controllers\Api;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -8,15 +8,36 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+ 
+    /* 
+        Defina ganchos para migrar la base de datos antes y después de cada prueba.
+    */
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function test_store()
+    {
+        // Llamada al manejador de errores para mostrar de manera más concisa la falla.
+        $this->withExceptionHandling();
+        /* 
+            Simulación de una aplicación esta llegando mediante la ruta
+            /api/posts y esta intentando guardar los datos mediante el
+            método post.
+        */
+        $response = $this->json('POST','/api/posts',[
+            'title' => 'El post de prueba'
+        ]);
+
+        /* 
+            Comprobar que se esta guardando los datos
+            ->assertJsonStructure() : confirmación de estructura
+            ->assertJson() : Confirmación de dato existente en Json
+            ->assertStatus(201) : Confirmación de status
+
+        */
+        $response->assertJsonStructure(['id','title','created_at','updated_at'])
+                 ->assertJson(['title' => 'El post de prueba'])
+                 ->assertStatus(201);
+
+        $this->assertDatabaseHas('posts',['title' => 'El post de prueba']);
     }
 }
